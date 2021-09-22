4e2fbf809761fbbe8026776d198374d315dadb28
KIMEUNBI01 <hufseunbi@gmail.com>
2021-09-23 02:49:14 +0900

    test

--- ListSortScan.py
+++ ListSortScan.py
@@ -1,4 +1,5 @@
 def two_sum(X,Y,t):
+    d
 
 # A 입력
 A = list(map(int,input().split()))
@@ -10,5 +11,5 @@ B = list(map(int,input().split()))
 C = list(map(int,input().split()))
 
 # 함수 two_sum을 적절한 형식으로 호출해 그 결과를 이용해 결과 출력
-
+ 
 # 수행시간 분석 및 Big-O 표기
